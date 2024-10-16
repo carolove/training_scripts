702496e55c267b4e7b45e7b976e76939db82c61b
chendan <doke.hi@gmail.com>
2024-10-16 15:48:33 +0800

    add ray trainer

--- transformers-llama-3.1/llama-3.1-8B/lora.py
+++ transformers-llama-3.1/llama-3.1-8B/lora.py
@@ -6,6 +6,11 @@ import json
 import os.path as osp
 from typing import Union
 
+
+import ray.train.huggingface.transformers
+from ray.train import ScalingConfig
+from ray.train.torch import TorchTrainer
+
 class Prompter(object):
     __slots__ = ("template", "_verbose")
 
@@ -46,94 +51,114 @@ class Prompter(object):
     def get_response(self, output: str) -> str:
         return output.split(self.template["response_split"])[1].strip()
 
+def train_func():
+    data = load_dataset("json", data_files="/workspace/training_scripts/transformers-llama-3.1/llama-3.1-8B/dataset/huanhuan.json")
+    prompter = Prompter("/workspace/training_scripts/transformers-llama-3.1/llama-3.1-8B/templates/alpaca.json")
 
-data = load_dataset("json", data_files="/workspace/training_scripts/transformers-llama-3.1/llama-3.1-8B/dataset/huanhuan.json")
-prompter = Prompter("/workspace/training_scripts/transformers-llama-3.1/llama-3.1-8B/templates/alpaca.json")
-
-tokenizer = AutoTokenizer.from_pretrained('/workspace/checkpoints/Meta-Llama-3___1-8B-Instruct', use_fast=False, trust_remote_code=True)
-tokenizer.pad_token_id = (
-    0  # unk. we want this to be different from the eos token
-)
-tokenizer.padding_side = "left"  # Allow batched inference
-
-def tokenize(prompt, add_eos_token=True):
-    # there's probably a way to do this with the tokenizer settings
-    # but again, gotta move fast
-    result = tokenizer(
-        prompt,
-        truncation=True,
-        max_length=256,
-        padding=False,
-        return_tensors=None,
+    tokenizer = AutoTokenizer.from_pretrained('/workspace/checkpoints/Meta-Llama-3___1-8B-Instruct', use_fast=False, trust_remote_code=True)
+    tokenizer.pad_token_id = (
+        0  # unk. we want this to be different from the eos token
     )
-    if (
-        result["input_ids"][-1] != tokenizer.eos_token_id
-        and len(result["input_ids"]) < 256
-        and add_eos_token
-    ):
-        result["input_ids"].append(tokenizer.eos_token_id)
-        result["attention_mask"].append(1)
+    tokenizer.padding_side = "left"  # Allow batched inference
 
-    result["labels"] = result["input_ids"].copy()
+    def tokenize(prompt, add_eos_token=True):
+        # there's probably a way to do this with the tokenizer settings
+        # but again, gotta move fast
+        result = tokenizer(
+            prompt,
+            truncation=True,
+            max_length=256,
+            padding=False,
+            return_tensors=None,
+        )
+        if (
+            result["input_ids"][-1] != tokenizer.eos_token_id
+            and len(result["input_ids"]) < 256
+            and add_eos_token
+        ):
+            result["input_ids"].append(tokenizer.eos_token_id)
+            result["attention_mask"].append(1)
 
-    return result
+        result["labels"] = result["input_ids"].copy()
 
-def generate_and_tokenize_prompt(data_point):
-    full_prompt = prompter.generate_prompt(
-        data_point["instruction"],
-        data_point["input"],
-        data_point["output"],
+        return result
+
+    def generate_and_tokenize_prompt(data_point):
+        full_prompt = prompter.generate_prompt(
+            data_point["instruction"],
+            data_point["input"],
+            data_point["output"],
+        )
+        tokenized_full_prompt = tokenize(full_prompt)
+        # could be sped up, probably
+        return tokenized_full_prompt
+
+
+    train_val = data["train"].train_test_split(
+        test_size=100, shuffle=True, seed=42
     )
-    tokenized_full_prompt = tokenize(full_prompt)
-    # could be sped up, probably
-    return tokenized_full_prompt
+    train_data = (
+        train_val["train"].shuffle().map(generate_and_tokenize_prompt)
+    )
+    val_data = (
+        train_val["test"].shuffle().map(generate_and_tokenize_prompt)
+    )
 
+    model = AutoModelForCausalLM.from_pretrained('/workspace/checkpoints/Meta-Llama-3___1-8B-Instruct', device_map="auto",torch_dtype=torch.bfloat16)
 
-train_val = data["train"].train_test_split(
-    test_size=100, shuffle=True, seed=42
+    model.enable_input_require_grads() # 开启梯度检查点时，要执行该方法
+
+    config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM, 
+        target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+        inference_mode=False, # 训练模式
+        r=8, # Lora 秩
+        lora_alpha=32, # Lora alaph，具体作用参见 Lora 原理
+        lora_dropout=0.1# Dropout 比例
+    )
+
+    model = get_peft_model(model, config)
+    model.print_trainable_parameters()
+
+    args = TrainingArguments(
+        output_dir="/workspace/checkpoints/llama3_1_instruct_lora",
+        per_device_train_batch_size=64,
+        gradient_accumulation_steps=4,
+        logging_steps=10,
+        num_train_epochs=3,
+        save_steps=10, # 为了快速演示，这里设置10，建议你设置成100
+        learning_rate=1e-4,
+        save_on_each_node=True,
+        gradient_checkpointing=True,
+        max_steps=20
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True),
+    )
+    # [2] Report Metrics and Checkpoints to Ray Train
+    # ===============================================
+    callback = ray.train.huggingface.transformers.RayTrainReportCallback()
+    trainer.add_callback(callback)
+
+    # [3] Prepare Transformers Trainer
+    # ================================
+    trainer = ray.train.huggingface.transformers.prepare_trainer(trainer)
+
+    trainer.train()
+
+# [4] Define a Ray TorchTrainer to launch `train_func` on all workers
+# ===================================================================
+ray_trainer = TorchTrainer(
+    train_func,
+    scaling_config=ScalingConfig(num_workers=1, use_gpu=True),
+    # [4a] If running in a multi-node cluster, this is where you
+    # should configure the run's persistent storage that is accessible
+    # across all worker nodes.
+    # run_config=ray.train.RunConfig(storage_path="s3://..."),
 )
-train_data = (
-    train_val["train"].shuffle().map(generate_and_tokenize_prompt)
-)
-val_data = (
-    train_val["test"].shuffle().map(generate_and_tokenize_prompt)
-)
-
-model = AutoModelForCausalLM.from_pretrained('/workspace/checkpoints/Meta-Llama-3___1-8B-Instruct', device_map="auto",torch_dtype=torch.bfloat16)
-
-model.enable_input_require_grads() # 开启梯度检查点时，要执行该方法
-
-config = LoraConfig(
-    task_type=TaskType.CAUSAL_LM, 
-    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
-    inference_mode=False, # 训练模式
-    r=8, # Lora 秩
-    lora_alpha=32, # Lora alaph，具体作用参见 Lora 原理
-    lora_dropout=0.1# Dropout 比例
-)
-
-model = get_peft_model(model, config)
-model.print_trainable_parameters()
-
-args = TrainingArguments(
-    output_dir="/workspace/checkpoints/llama3_1_instruct_lora",
-    per_device_train_batch_size=64,
-    gradient_accumulation_steps=4,
-    logging_steps=10,
-    num_train_epochs=3,
-    save_steps=10, # 为了快速演示，这里设置10，建议你设置成100
-    learning_rate=1e-4,
-    save_on_each_node=True,
-    gradient_checkpointing=True,
-    max_steps=20
-)
-
-trainer = Trainer(
-    model=model,
-    args=args,
-    train_dataset=train_data,
-    eval_dataset=val_data,
-    data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True),
-)
-
-trainer.train()
+result: ray.train.Result = ray_trainer.fit()
