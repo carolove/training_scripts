ad9d1365d83b26024e49dd519aba92c3fedbd0b5
chendan <doke.hi@gmail.com>
2024-10-16 15:27:34 +0800

    add alpaca data tokenizer into lora

--- transformers-llama-3.1/llama-3.1-8B/lora.py
+++ transformers-llama-3.1/llama-3.1-8B/lora.py
@@ -1,58 +1,65 @@
-from datasets import Dataset
-import pandas as pd
+from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorForSeq2Seq, TrainingArguments, Trainer, GenerationConfig
+from peft import LoraConfig, TaskType, get_peft_model
+import torch
+from utils.prompter import Prompter
 
-df = pd.read_json('dataset/huanhuan.json')
-ds = Dataset.from_pandas(df)
-
-print(ds[:3])
+data = load_dataset("json", data_files="/workspace/training_scripts/transformers-llama-3.1/llama-3.1-8B/dataset/huanhuan.json")
+prompter = Prompter("alpaca")
 
 tokenizer = AutoTokenizer.from_pretrained('/workspace/checkpoints/Meta-Llama-3___1-8B-Instruct', use_fast=False, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
+tokenizer.pad_token_id = (
+    0  # unk. we want this to be different from the eos token
+)
+tokenizer.padding_side = "left"  # Allow batched inference
 
-messages = [
-        {"role": "system", "content": "现在你要扮演皇帝身边的女人--甄嬛"},
-        {"role": "user", "content": '你好呀'},
-        {"role": "assistant", "content": "你好，我是甄嬛，你有什么事情要问我吗？"},    
-    ]
-print(tokenizer.apply_chat_template(messages, tokenize=False))
+def tokenize(prompt, add_eos_token=True):
+    # there's probably a way to do this with the tokenizer settings
+    # but again, gotta move fast
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=256,
+        padding=False,
+        return_tensors=None,
+    )
+    if (
+        result["input_ids"][-1] != tokenizer.eos_token_id
+        and len(result["input_ids"]) < 256
+        and add_eos_token
+    ):
+        result["input_ids"].append(tokenizer.eos_token_id)
+        result["attention_mask"].append(1)
 
-def process_func(example):
-    MAX_LENGTH = 384    # Llama分词器会将一个中文字切分为多个token，因此需要放开一些最大长度，保证数据的完整性
-    input_ids, attention_mask, labels = [], [], []
-    instruction = tokenizer(f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nCutting Knowledge Date: December 2023\nToday Date: 26 Jul 2024\n\n现在你要扮演皇帝身边的女人--甄嬛<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{example['instruction'] + example['input']}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n", add_special_tokens=False)  # add_special_tokens 不在开头加 special_tokens
-    response = tokenizer(f"{example['output']}<|eot_id|>", add_special_tokens=False)
-    input_ids = instruction["input_ids"] + response["input_ids"] + [tokenizer.pad_token_id]
-    attention_mask = instruction["attention_mask"] + response["attention_mask"] + [1]  # 因为eos token咱们也是要关注的所以 补充为1
-    labels = [-100] * len(instruction["input_ids"]) + response["input_ids"] + [tokenizer.pad_token_id]  
-    if len(input_ids) > MAX_LENGTH:  # 做一个截断
-        input_ids = input_ids[:MAX_LENGTH]
-        attention_mask = attention_mask[:MAX_LENGTH]
-        labels = labels[:MAX_LENGTH]
-    return {
-        "input_ids": input_ids,
-        "attention_mask": attention_mask,
-        "labels": labels
-    }
+    result["labels"] = result["input_ids"].copy()
 
-tokenized_id = ds.map(process_func, remove_columns=ds.column_names)
-tokenized_id
+    return result
 
-tokenizer.decode(tokenized_id[0]['input_ids'])
+def generate_and_tokenize_prompt(data_point):
+    full_prompt = prompter.generate_prompt(
+        data_point["instruction"],
+        data_point["input"],
+        data_point["output"],
+    )
+    tokenized_full_prompt = tokenize(full_prompt)
+    # could be sped up, probably
+    return tokenized_full_prompt
 
-tokenizer.decode(list(filter(lambda x: x != -100, tokenized_id[1]["labels"])))
 
-import torch
+train_val = data["train"].train_test_split(
+    test_size=100, shuffle=True, seed=42
+)
+train_data = (
+    train_val["train"].shuffle().map(generate_and_tokenize_prompt)
+)
+val_data = (
+    train_val["test"].shuffle().map(generate_and_tokenize_prompt)
+)
 
 model = AutoModelForCausalLM.from_pretrained('/workspace/checkpoints/Meta-Llama-3___1-8B-Instruct', device_map="auto",torch_dtype=torch.bfloat16)
 
-model
-
 model.enable_input_require_grads() # 开启梯度检查点时，要执行该方法
-
-model.dtype
-
-from peft import LoraConfig, TaskType, get_peft_model
+model.print_trainable_parameters()
 
 config = LoraConfig(
     task_type=TaskType.CAUSAL_LM, 
@@ -63,13 +70,7 @@ config = LoraConfig(
     lora_dropout=0.1# Dropout 比例
 )
 
-config
-
 model = get_peft_model(model, config)
-
-config
-
-model.print_trainable_parameters()
 
 args = TrainingArguments(
     output_dir="/workspace/checkpoints/llama3_1_instruct_lora",
@@ -87,7 +88,8 @@ args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=args,
-    train_dataset=tokenized_id,
+    train_dataset=train_data,
+    eval_dataset=val_data,
     data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True),
 )
 
