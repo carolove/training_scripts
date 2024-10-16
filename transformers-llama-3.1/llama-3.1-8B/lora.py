6208e23d0cf63abef7254b8632da985b44a0ca3b
chendan <doke.hi@gmail.com>
2024-10-16 15:36:20 +0800

    move prompter into single file

--- transformers-llama-3.1/llama-3.1-8B/lora.py
+++ transformers-llama-3.1/llama-3.1-8B/lora.py
@@ -2,7 +2,53 @@ from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorForSeq2Seq, TrainingArguments, Trainer, GenerationConfig
 from peft import LoraConfig, TaskType, get_peft_model
 import torch
-from utils.prompter import Prompter
+import json
+import os.path as osp
+from typing import Union
+
+class Prompter(object):
+    __slots__ = ("template", "_verbose")
+
+    def __init__(self, template_name: str = "", verbose: bool = False):
+        self._verbose = verbose
+        if not template_name:
+            # Enforce the default here, so the constructor can be called with '' and will not break.
+            template_name = "alpaca"
+        file_name = osp.join("templates", f"{template_name}.json")
+        if not osp.exists(file_name):
+            raise ValueError(f"Can't read {file_name}")
+        with open(file_name) as fp:
+            self.template = json.load(fp)
+        if self._verbose:
+            print(
+                f"Using prompt template {template_name}: {self.template['description']}"
+            )
+
+    def generate_prompt(
+        self,
+        instruction: str,
+        input: Union[None, str] = None,
+        label: Union[None, str] = None,
+    ) -> str:
+        # returns the full prompt from instruction and optional input
+        # if a label (=response, =output) is provided, it's also appended.
+        if input:
+            res = self.template["prompt_input"].format(
+                instruction=instruction, input=input
+            )
+        else:
+            res = self.template["prompt_no_input"].format(
+                instruction=instruction
+            )
+        if label:
+            res = f"{res}{label}"
+        if self._verbose:
+            print(res)
+        return res
+
+    def get_response(self, output: str) -> str:
+        return output.split(self.template["response_split"])[1].strip()
+
 
 data = load_dataset("json", data_files="/workspace/training_scripts/transformers-llama-3.1/llama-3.1-8B/dataset/huanhuan.json")
 prompter = Prompter("alpaca")
@@ -59,7 +105,6 @@ val_data = (
 model = AutoModelForCausalLM.from_pretrained('/workspace/checkpoints/Meta-Llama-3___1-8B-Instruct', device_map="auto",torch_dtype=torch.bfloat16)
 
 model.enable_input_require_grads() # 开启梯度检查点时，要执行该方法
-model.print_trainable_parameters()
 
 config = LoraConfig(
     task_type=TaskType.CAUSAL_LM, 
@@ -71,6 +116,7 @@ config = LoraConfig(
 )
 
 model = get_peft_model(model, config)
+model.print_trainable_parameters()
 
 args = TrainingArguments(
     output_dir="/workspace/checkpoints/llama3_1_instruct_lora",
